5036d9bc1ca5d5db5daeb3a119ab40095f6042bb
hiro032 <kiryuinh32@gmail.com>
2022-10-22 19:09:41 +0900

    feat: token invalid error code

--- src/main/java/com/hiro/sns/exception/ErrorCode.java
+++ src/main/java/com/hiro/sns/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "user name is duplicated"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error"),
     WRONG_PASSWORD(HttpStatus.CONFLICT, "user password is wrong"),
-    USER_NOT_FOUND(HttpStatus.CONFLICT, "not found user");
+    USER_NOT_FOUND(HttpStatus.CONFLICT, "not found user"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "token is invalid");
 
     private final HttpStatus httpStatus;
     private final String message;
